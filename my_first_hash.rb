f0b5ac009f08c98b751fa3cb67052092842f1576
james hall <halljames631@gmail.com>
2019-12-12 19:27:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,7 +32,7 @@ def adding
                   "gun powder"=>4, 
                   "muskets"=>2, 
                   "oil paintings"=>3, 
-                  "porcelainvases"=>2, 
+                  "porcelain vases"=>2, 
                   "whale bone corsets"=>5
                  }
   # add 2 muskets to the shipping_manifest hash below
