9cfe4fe856dcad7ea8ae30f90982a1a5af987c70
Asha Fernandez <ash.fernan90@gmail.com>
2019-12-21 06:01:12 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,38 +1,26 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, #equal to a hash with key/value pairs of your choice.
+require 'pry'
 
+def my_hash
   your_hash = {"name" => "Asha"}
 end
 
 def shipping_manifest
-  
-  the_manifest = {
-      "whale bone corsets" => 5, 
-      "porcelain vases" => 2, 
-      "oil paintings" => 3
-    }
+
+  the_manifest= { "whale bone corsets" =>5, "porcelain vases" =>2, "oil paintings" =>3 }
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-   shipping_manifest hash below
- end
+  shipping_manifest= { "whale bone corsets" =>5, "porcelain vases" =>2, "oil paintings" =>3 }
+  
   shipping_manifest["oil paintings"]
 end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+  shipping_manifest ={ "whale bone corsets" =>5, "porcelain vases" =>2, "oil paintings" =>3 }
+
   shipping_manifest["muskets"] = 2
 
   shipping_manifest["gun powder"] = 4
-end
+  
   shipping_manifest
 end
